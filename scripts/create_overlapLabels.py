1ec71a15f59cdd08ec276fa8cf345fea0a403f19
Angad Kalra <o1c9@ugrad.cs.ubc.ca>
2017-08-15 14:15:32 -0700

    created labels using overlapping percentage.

---
+++ scripts/create_overlapLabels.py
@@ -0,0 +1,51 @@
+import csv
+import numpy as np
+
+# Read data from csv files and turn into dictionaries for easy further processing
+foxp3Seqs = {}
+atacSeqs = {}
+labels = np.zeros(63197)
+
+with open('../data/original/Foxp3.ChIPseq.csv') as foxp3:
+    reader = csv.reader(foxp3, delimiter=',')
+    next(reader)    # skip column headers
+
+    for row in reader:
+        if row[0] in foxp3Seqs:
+            foxp3Seqs[row[0]].append((row[1], row[2]))
+        else:
+            foxp3Seqs[row[0]] = [(row[1], row[2])]
+
+with open('../data/original/dpz.SplTreg.ATAC.density_GCandquantile.csv') as atac:
+    reader = csv.reader(atac, delimiter=',')
+    next(reader)    # skip column headers
+
+    i = 1
+    for row in reader:
+        if row[0] in atacSeqs:
+            atacSeqs[row[0]].append((row[1], row[2], 'peak_' + str(i)))
+        else:
+            atacSeqs[row[0]] = [(row[1], row[2], 'peak_' + str(i))]
+
+        i = i + 1
+
+
+for chrm, rangeList in foxp3Seqs.items():
+
+    if chrm in atacSeqs.keys():
+        for r1 in rangeList:
+            s1, e1 = int(r1[0]), int(r1[1])
+
+            for r2 in atacSeqs[chrm]:
+                s2, e2 = int(r2[0]), int(r2[1])
+                peakLabel = r2[2]
+                peakNum = int(peakLabel[5:])
+
+                if max(s1, s2) <= min(e1, e2):
+                    overlap = np.abs(min(e1,e2) - max(s1,s2))
+                    if overlap == 0:
+                        overlap = 1
+
+                    labels[peakNum - 1] = labels[peakNum - 1] + overlap/251
+
+np.savetxt('../data/overlapLabels', labels)
